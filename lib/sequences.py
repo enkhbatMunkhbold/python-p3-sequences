b6552ed669f13cd3fd44efe48d363837f55be772
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2024-07-28 21:48:18 -0700

    Complete Assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
-def print_fibonacci(length):
-    pass
+def print_fibonacci(length = 0):
+    nums = []
+    for index in range(length):
+        if(index < 2):
+            nums.append(index)
+        else:
+            nums.append(nums[-2] + nums[-1])    
+    print(nums)
+
+print_fibonacci(3)
